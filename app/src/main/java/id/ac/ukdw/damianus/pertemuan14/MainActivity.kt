e56f682368e5b2c2f1103a8e888b141d055e8f62
DeanDamianus <damianus.atisanta@ti.ukdw.ac.id>
2022-12-08 15:14:44 +0700

    tes

---
+++ app/src/main/java/id/ac/ukdw/damianus/pertemuan14/MainActivity.kt
@@ -0,0 +1,14 @@
+package id.ac.ukdw.damianus.pertemuan14
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
