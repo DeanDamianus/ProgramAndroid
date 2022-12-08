d1019101a1fd172c1b2f681e3a4fc1e4ddaef380
DeanDamianus <damianus.atisanta@ti.ukdw.ac.id>
2022-12-08 15:40:55 +0700

    main

--- app/src/main/java/id/ac/ukdw/damianus/pertemuan14/MainActivity2.kt
+++ app/src/main/java/id/ac/ukdw/damianus/pertemuan14/MainActivity2.kt
@@ -1,11 +1,31 @@
 package id.ac.ukdw.damianus.pertemuan14
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioGroup
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val waktu = findViewById<RadioGroup>(R.id.waktu)
+        val kelas = findViewById<RadioGroup>(R.id.tipe)
+
+        val submit2 = findViewById<Button>(R.id.button2)
+        submit2.setOnClickListener {
+            val kotaAsal = waktu.toString()
+            val kotaTujuan = kelas.toString()
+
+
+            val intent = Intent(this, hasil::class.java)
+
+            intent.putExtra("Ini Waktu keberangkatan Anda : ", kotaAsal)
+            intent.putExtra("Ini Kelas Tiket Anda : ", kotaTujuan)
+
+            startActivity(intent)
+        }
     }
 }
